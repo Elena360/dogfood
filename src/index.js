fc55a66a209639827e8232d77ab9b56f10c425aa
Elena <lenoktimur@mail.ru>
2022-11-02 11:32:10 +0300

    Create first App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
-ReactDOM.render(
-	<App/>,
+import reactDom from "react-dom";
+import "./index.css"
+reactDom.render(
+	<div className="wrapper">
+		<div className="card">=)</div>
+		<div className="card">O_o</div>
+		<div className="card">^_^</div>
+		<div className="card">=(</div>
+	</div>,
 	document.querySelector("#root")
-);
+)
